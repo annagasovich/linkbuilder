5291cb0e150aa761a86cac02b6f4631d6043ba1d
a_gasovich <a.gasovich@autodrive.ru>
2021-07-28 18:49:41 +0300

    Бэкапы базы 2

--- app/services/Backup.php
+++ app/services/Backup.php
@@ -11,7 +11,7 @@ class Backup
     {
         $filename='database_backup_'.date('G_a_m_d_y').'.sql';
 
-        $result=exec('mysqldump '.MYSQL_DATABASE.' --password='.MYSQL_PASSWORD.' --user='.MYSQL_USER.' --single-transaction >'.DOCROOT.'backups/'.$filename,$output);
+        $result=exec('mysqldump '.MYSQL_DATABASE.' -h127.0.0.1 --port=3310 --password='.MYSQL_PASSWORD.' --user='.MYSQL_USER.' --single-transaction >'.DOCROOT.'backups/'.$filename,$output);
     }
 
 
